a733c4583a2d86c29d04f849220f7fe05a9703df
lass6230 <due6230@gmail.com>
2020-12-23 00:39:32 +0100

    h

--- src/node_2.cpp
+++ src/node_2.cpp
@@ -1,5 +1,4 @@
 #include <iostream>
-<<<<<<< HEAD
 #include <ros/ros.h>  
 #include <move_base_msgs/MoveBaseAction.h>
 #include <actionlib/client/simple_action_client.h>
@@ -30,20 +29,10 @@ using namespace std;
 
 // }
 
-=======
-#include "ros/ros.h"
-#include <ros/ros.h>
-#include <string>
-#include <std_msgs/String.h>
-using namespace std;
-ros::NodeHandle n;
-void pub(int i);
->>>>>>> b541e4b98d695655da08bfd3b1c99e727dd944bf
 
 int main(int argc, char **argv)
 {
     ros::init(argc, argv, "node_2");
-<<<<<<< HEAD
     ros::NodeHandle n;
     // ros::ServiceServer service = n.advertiseService("myString",text);
     ROS_INFO("Ready to do action.");
@@ -79,23 +68,3 @@ int main(int argc, char **argv)
     ros::spinOnce();
     return 0;
 }
-=======
-    //ros::NodeHandle n;
-    ros::spinOnce();
-return 0;
-
-}
-
-void pub(int i){
-
-    ros::Publisher int_pub = n.advertise<std_msgs::String>("int_chat", 1000);
-    std_msgs::String msg;
-    stringstream msg_data;
-    msg_data << "davs";
-    msg.data = msg_data.str();
-    ROS_INFO("%S", msg.data.c_str());
-    int_pub.publish(msg);
-    ros::spinOnce();
-
-}
->>>>>>> b541e4b98d695655da08bfd3b1c99e727dd944bf
